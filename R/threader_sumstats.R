2c3cd53a85d1ce327f05ec37004f38e87f1d721b
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2023-03-26 17:55:59 -0700

    threader_sumstats() now allows grouping

--- R/threader_sumstats.R
+++ R/threader_sumstats.R
@@ -9,7 +9,7 @@
 #'
 #' @examples
 #' grid_sumstats <- threader_sumstats()
-threader_sumstats <- function(grid_long_fpath = NULL) {
+threader_sumstats <- function(grid_long_fpath = NULL, group_by_var = NULL) {
   if (is.null(grid_long_fpath)) {
     grid_long_fpath <- demo_grid_long_fpath()
   }
@@ -23,13 +23,25 @@ threader_sumstats <- function(grid_long_fpath = NULL) {
     dplyr::count() %>%
     dplyr::pull()
   num_null <- num_obs - num_nonnull
+  # Now we do the groupby stats, if requested
+  num_groups <- NA
+  group_message <- NULL
+  if (!is.null(group_by_var)) {
+    group_df <- num_df %>% dplyr::group_by(!!as.symbol(group_by_var))
+    num_groups <- group_df %>% dplyr::n_groups()
+    group_message <- paste0("Grouping by ",group_by_var,": ",num_groups," groups")
+  }
   sumstats <- list(
     num_obs=num_obs,
     mean_est=mean_est,
     num_nonnull=num_nonnull,
-    num_null=num_null
+    num_null=num_null,
+    num_groups=num_groups
   )
   message(paste0("N = ",num_obs," (",num_nonnull," non-NA, ",num_null," NA)"))
   message(paste0("Mean Estimate = ",mean_est))
+  if (!is.null(group_message)) {
+    message(group_message)
+  }
   return(sumstats)
 }
